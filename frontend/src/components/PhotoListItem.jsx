2532fb2bb29a4032ef34b6b1d82e2a55ca2a7619
ahmaddaadaa <ahmadd@uvic.ca>
2023-08-09 16:35:15 -0700

    constructed the HTML containers for showing the photo item data

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,26 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
+
+const PhotoListItem = (props) => {
   /* Insert React */
+  const { location, username, profile, imageSource } = props.data;
+
+  return (
+    <div className="photo-list-item">
+    <img src={imageSource} />
+
+    <div className="photographer">
+      <img src={profile} />
+      <span className="username">{username}</span>
+    </div>
+
+    <div className="location">
+      <span className="city">{location.city} </span>
+      <span className="country">{location.country}</span>
+    </div>
+  </div>);
 };
 
 export default PhotoListItem;
