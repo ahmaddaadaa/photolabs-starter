22b9f8d24a65b5ef193d43a155cc7aa6d0005612
ahmaddaadaa <ahmadd@uvic.ca>
2023-08-15 02:08:13 -0700

    Optimized code and implemented prop passing in components for app functionality

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -2,13 +2,10 @@ import React, { useEffect, useState } from "react";
 import '../styles/HomeRoute.scss';
 import TopNavigationBar from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
-import PhotoDetailsModal from "./PhotoDetailsModal";
-import useHandleClicks from "../hooks/useHandleClicks";
+
 
 
 const HomeRoute = (props) => {
-  const { favList, handleClicks } = useHandleClicks();
-  const [dataFromModal, updateDataFromModal] = useState(null);
 
   const handleDataFromPhotoList = (data) => {
     props.sendDataToApp(data);
@@ -18,29 +15,16 @@ const HomeRoute = (props) => {
     props.sendObjToApp(data);
   };
 
-  useEffect(() => {
-    if (props.dataFromModal !== dataFromModal ) {
-      updateDataFromModal(props.dataFromModal);
-    }
-
-    // Now you can update the favList based on the updated dataFromModal
-    if (dataFromModal) {
-      handleClicks(dataFromModal);
-    }
-
-  }, [props.dataFromModal, dataFromModal]);
-
-  console.log("dataFromModal: ",dataFromModal);
   
   return (
     <>
       <div>
-        <TopNavigationBar isFavPhotoExist={Object.values(favList).some((fav) => fav)} />
+        <TopNavigationBar isFavPhotoExist={props.favListArray.length > 0} />
         <div className="home-route">
           <div className="photo-list">
             <PhotoList
-              handleClicks={handleClicks}
-              favList={favList}
+              favListArray={props.favListArray}
+              favListFunction={props.favListFunction}
               sendDataToHomeRoute={handleDataFromPhotoList}
               sendObjToHomeRoute={handleObjFromPhotoList}
             />
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,64 +1,77 @@
 import React, { useEffect, useState } from "react";
-useEffect;
-
 import "../styles/PhotoDetailsModal.scss";
 import closeSymbol from "../assets/closeSymbol.svg";
-import FavIcon from "components/FavIcon";
 import PhotoList from "components/PhotoList";
 import "../styles/PhotoListItem.scss";
 import "../styles/FavBadge.scss";
-import "../styles/PhotoList.scss"
+import "../styles/PhotoList.scss";
 import FavButton from "components/FavButton";
+
 
 const PhotoDetailsModal = (props) => {
   const receivedObj = props.objFromApp;
-  
+
   const receivedData = props.dataFromApp;
   const [buttonStatus, setStatus] = useState(receivedData);
   const handleCloseButton = function () {
     setStatus(!buttonStatus);
     props.resetData();
   };
+
   useEffect(() => {
     setStatus(receivedData);
   }, [receivedData]);
-  
- 
-  const handleFavClickInModal = () => {
-    // props.handleClicksInModal("hello From Modal");
-    // props.onDataFromModal("hello"); // Sending "hello" back to HomeRoute
-    console.log("you clicked Fav inside pop up");
-    props.onDataFromModal(receivedObj.id);
-  };
- 
+
   return (
     <div className={`photo-details-modal ${buttonStatus ? "" : "hide"}`}>
-      <button
-        className="photo-details-modal__close-button"
-        onClick={handleCloseButton}
-      >
-        <img src={closeSymbol} alt="close symbol" />
-      </button>
+      <div className="column">
+      <div>
+        <button
+          className="photo-details-modal__close-button"
+          onClick={handleCloseButton}
+        >
+          <img src={closeSymbol} alt="close symbol" />
+        </button>
+      </div>
 
-      <div className="photo-list__item_window">
-        <div className="fav-badge">
-    
-        <FavButton selected={buttonStatus} onClick={handleFavClickInModal} />
-     
+      <div>
+        <div className="photo-list__item_window">
+          <div className="fav-badge">
+            <FavButton
+              selected={props.favListArray.includes(receivedObj.id)}
+              onClick={() => props.favListFunction(receivedObj.id)}
+            />
+          </div>
+          <img className="big-top-photo" src={receivedObj.urls.regular} />
         </div>
-        <img
-          className="big-top-photo"
-          src={receivedObj.urls.regular}
-        />
       </div>
-      <h5 className="title">Similar Photos</h5>
-      <div className="list-container">
-      <img className="small-photos-list" src={receivedObj.urls.regular}/>
-      <img className="small-photos-list" src={receivedObj.urls.regular}/>
-      <img className="small-photos-list" src={receivedObj.urls.regular}/>
 
+      <div className="photo-list__bottomLine">
+        <div className="profile">
+          <img className="photo-list__user-profile" src={receivedObj.user.profile} />
+        </div>
+        <div className="photo-list__user-details">
+          <span className="photo-list__user-info">{receivedObj.user.name}</span>
+          <div className="photo-list__user-location">
+            <span>{receivedObj.location.city} </span>
+            <span>{receivedObj.location.country}</span>
+          </div>
+        </div>
       </div>
-    </div>
+      <div className="line">    </div>
+      <h5 className="title">Related Photos</h5>
+      <div className="item-container">
+        </div>
+        </div>
+        <PhotoList
+          favListArray={props.favListArray}
+          favListFunction={props.favListFunction}
+          differentPage={true}
+          excludedPhotoId={receivedObj.id} 
+        />
+     </div>
+  
+
   );
 };
 
