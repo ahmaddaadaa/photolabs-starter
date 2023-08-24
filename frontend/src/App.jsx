3cc166d432b57143ddaf7615c0159f8663fc7bbc
ahmaddaadaa <ahmadd@uvic.ca>
2023-08-24 14:54:09 -0700

    Refactored the code to import primary states and functions from useApplicationData hook and passed updated props to components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,89 +1,43 @@
-import React, { useReducer, useState } from "react";
-import './App.scss';
-import HomeRoute from 'routes/HomeRoute';
-import PhotoDetailsModal from 'routes/PhotoDetailsModal';
-import useFavList from "hooks/useFavList";
-
-const favListReducer = (state, action) => {
-  switch (action.type) {
-    case "ADD":
-      if (!state.includes(action.id)) {
-        return [...state, action.id];
-      }
-      return state;
-    case "REMOVE":
-      return state.filter((id) => id !== action.id);
-    default:
-      return state;
-  }
-};
+import React from "react";
+import "./App.scss";
+import HomeRoute from "routes/HomeRoute";
+import PhotoDetailsModal from "routes/PhotoDetailsModal";
+import useApplicationData from "hooks/useApplicationData";
 
 const App = () => {
-  
-  //const { favList, updateToFavPhotoIds } = useFavList();
-
-  const [favList, dispatchFavList] = useReducer(favListReducer, []);
-
-  const updateFavList = function (id) {
-    if (favList.includes(id)) {
-      dispatchFavList({ type: "REMOVE", id });
-    } else {
-      dispatchFavList({ type: "ADD", id });
-    }
-  };
-
-
-  const [receivedData, setReceivedData] = useState(null);
-  const [receivedObj, setReceivedObj] = useState({
-    "id": "7",
-    "location": {
-      "city": "Calgary",
-      "country": "Canada"
-    },
-    "urls": {
-      "full": `${process.env.PUBLIC_URL}/Image-7-Full.jpg`,
-      "regular": `${process.env.PUBLIC_URL}/Image-7-Regular.jpg`
-    },
-    "user": {
-      "id": "7",
-      "username": "exampleuser",
-      "name": "Joe Example",
-      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
-    }
-  });
-
-   // Callback function to receive data from the HomeRoute
-   const handleDataFromHomeRoute = (data) => {
-    setReceivedData(data);
-  };
-  const resetDataFromApp = () => {
-    setReceivedData(false); // Reset dataFromApp to false
-  };
-  const handleObjFromHomeRoute = (data) => {
-    setReceivedObj(data);
-  };
-
+  const {
+    favList,
+    updateFavList,
+    topicListData,
+    photoData,
+    handleTopicClick,
+    receivedData,
+    receivedObj,
+    handleDataFromHomeRoute,
+    resetDataFromApp,
+    handleObjFromHomeRoute,
+  } = useApplicationData();
 
   return (
     <>
-    <HomeRoute 
-      sendDataToApp={handleDataFromHomeRoute} 
-      sendObjToApp={handleObjFromHomeRoute}
-      favListArray={favList}
-      favListFunction={updateFavList}
+      <HomeRoute
+        handleTopicClick={handleTopicClick}
+        photoData={photoData}
+        topicListData={topicListData}
+        sendDataToApp={handleDataFromHomeRoute}
+        sendObjToApp={handleObjFromHomeRoute}
+        favListArray={favList}
+        favListFunction={updateFavList}
+      />
 
+      <PhotoDetailsModal
+        dataFromApp={receivedData}
+        objFromApp={receivedObj}
+        resetData={resetDataFromApp}
+        favListArray={favList}
+        favListFunction={updateFavList}
       />
-    <PhotoDetailsModal 
-      dataFromApp={receivedData}
-      objFromApp={receivedObj} 
-      resetData={resetDataFromApp}
-      favListArray={favList}
-      favListFunction={updateFavList}
-      />
-      
     </>
-
-    
   );
 };
 
